1e2fe164245910d9e5cd85262939e89a43cc5946
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-04-28 20:28:37 +0800

    index.js

--- function/index.js
+++ function/index.js
@@ -2,5 +2,7 @@ module.exports = {
 	blackboxAIChat: require('./scraper/blackboxAIChat'),
 	smartContract: require('./scraper/smartContract'),
 	ragBot: require('./scraper/ragBot'),
+	tiktok2: require('./scraper/user'),
+	tiktokserch2: require('./scraper/Viralkan'),
 	degreeGuru: require('./scraper/degreeGuru')
 }
