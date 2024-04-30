b7bb6edf28508ceb0ebdb79a0c4ab425f9d46ca9
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-01 06:49:39 +0800

    index.js

--- index.js
+++ index.js
@@ -278,7 +278,6 @@ app.get('/api/ytmp3', async (req, res) => {
   }
 });
 app.get('/api/spotify', async (req, res) => {
-  try {
     const message = req.query.url;
     if (!message) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
@@ -293,7 +292,7 @@ app.get('/api/spotify', async (req, res) => {
       result 
     });
         }
-  } catch (error) {
+  .catch(e => {
   res.status(500).json({ error: error.message });
   }
 });
