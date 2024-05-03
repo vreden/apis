ab2054c44d94f067f2cfd91b633fb0980ee6c2b0
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-03 20:47:43 +0800

    index.js

--- index.js
+++ index.js
@@ -438,11 +438,10 @@ app.get('/api/capcutdl', async (req, res) => {
     if (!message) {
       return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
     }
-     let res4 = await capcut(message);
+     var res4 = await capcut(message);
     res.status(200).json({
       status: 200,
       creator: "RIAN X EXONITY",
-      res4.thumbnail 
       res4.video                   
     });
     })
