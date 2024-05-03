25621be788e877788d2f4f4ded1214372ee4e706
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-03 20:15:14 +0800

    Add files via upload

---
+++ function/scraper/exonityscraper.js
@@ -0,0 +1,78 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+
+function quotesAnime() {
+    return new Promise((resolve, reject) => {
+        const page = Math.floor(Math.random() * 184)
+axios.get('https://otakotaku.com/quote/feed/'+page)
+        .then(({ data }) => {
+            const $ = cheerio.load(data)
+            const hasil = []
+            $('div.kotodama-list').each(function(l, h) {
+                hasil.push({
+                    link: $(h).find('a').attr('href'),
+                    gambar: $(h).find('img').attr('data-src'),
+                    karakter: $(h).find('div.char-name').text().trim(),
+                    anime: $(h).find('div.anime-title').text().trim(),
+                    episode: $(h).find('div.meta').text(),
+                    up_at: $(h).find('small.meta').text(),
+                    quotes: $(h).find('div.quote').text().trim()
+
+                })
+
+            })
+
+            resolve(hasil)
+
+        }).catch(reject)
+
+    })
+
+}
+
+
+async function capcut(url) {
+  const response = await fetch(url);
+  const data = await response.text();
+  const $ = cheerio.load(data);
+  return {
+    thumbnail: $("video").attr("poster"),
+    video: $("video").attr("src"),
+  };
+
+}
+async function chatgptss(message) {
+    const url = 'https://chatgptss.org';
+    const formData = new FormData();
+
+    try {
+        const html = await (await fetch(url)).text();
+        const $ = cheerio.load(html);
+
+        const chatData = $('.wpaicg-chat-shortcode').map((index, element) => {
+            return Object.fromEntries(Object.entries(element.attribs));
+        }).get();
+
+        formData.append('_wpnonce', chatData[0]['data-nonce']);
+        formData.append('post_id', chatData[0]['data-post-id']);
+        formData.append('action', 'wpaicg_chatbox_message');
+        formData.append('message', message);
+
+        const response = await fetch('https://chatgptss.org/wp-admin/admin-ajax.php', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) throw new Error('Network response was not ok');
+
+        let resultan = await response.json();
+        return resultan.data
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+        throw error;
+    }
+}
+module.exports = { quotesAnime, 
+chatgptss, capcut}
