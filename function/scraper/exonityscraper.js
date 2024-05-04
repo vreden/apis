9846740767e2236b1fb39f9f4d59c05d1e7719ca
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-04 17:10:07 +0800

    exonityscraper.js

--- function/scraper/exonityscraper.js
+++ function/scraper/exonityscraper.js
@@ -31,8 +31,54 @@ axios.get('https://otakotaku.com/quote/feed/'+page)
     })
 
 }
-
-
+function pinterest(querry){
+	return new Promise(async(resolve,reject) => {
+		 axios.get('https://id.pinterest.com/search/pins/?autologin=true&q=' + querry, {
+			headers: {
+			"cookie" : "_auth=1; _b=\"AVna7S1p7l1C5I9u0+nR3YzijpvXOPc6d09SyCzO+DcwpersQH36SmGiYfymBKhZcGg=\"; _pinterest_sess=TWc9PSZHamJOZ0JobUFiSEpSN3Z4a2NsMk9wZ3gxL1NSc2k2NkFLaUw5bVY5cXR5alZHR0gxY2h2MVZDZlNQalNpUUJFRVR5L3NlYy9JZkthekp3bHo5bXFuaFZzVHJFMnkrR3lTbm56U3YvQXBBTW96VUgzVUhuK1Z4VURGKzczUi9hNHdDeTJ5Y2pBTmxhc2owZ2hkSGlDemtUSnYvVXh5dDNkaDN3TjZCTk8ycTdHRHVsOFg2b2NQWCtpOWxqeDNjNkk3cS85MkhhSklSb0hwTnZvZVFyZmJEUllwbG9UVnpCYVNTRzZxOXNJcmduOVc4aURtM3NtRFo3STlmWjJvSjlWTU5ITzg0VUg1NGhOTEZzME9SNFNhVWJRWjRJK3pGMFA4Q3UvcHBnWHdaYXZpa2FUNkx6Z3RNQjEzTFJEOHZoaHRvazc1c1UrYlRuUmdKcDg3ZEY4cjNtZlBLRTRBZjNYK0lPTXZJTzQ5dU8ybDdVS015bWJKT0tjTWYyRlBzclpiamdsNmtpeUZnRjlwVGJXUmdOMXdTUkFHRWloVjBMR0JlTE5YcmhxVHdoNzFHbDZ0YmFHZ1VLQXU1QnpkM1FqUTNMTnhYb3VKeDVGbnhNSkdkNXFSMXQybjRGL3pyZXRLR0ZTc0xHZ0JvbTJCNnAzQzE0cW1WTndIK0trY05HV1gxS09NRktadnFCSDR2YzBoWmRiUGZiWXFQNjcwWmZhaDZQRm1UbzNxc21pV1p5WDlabm1UWGQzanc1SGlrZXB1bDVDWXQvUis3elN2SVFDbm1DSVE5Z0d4YW1sa2hsSkZJb1h0MTFpck5BdDR0d0lZOW1Pa2RDVzNySWpXWmUwOUFhQmFSVUpaOFQ3WlhOQldNMkExeDIvMjZHeXdnNjdMYWdiQUhUSEFBUlhUVTdBMThRRmh1ekJMYWZ2YTJkNlg0cmFCdnU2WEpwcXlPOVZYcGNhNkZDd051S3lGZmo0eHV0ZE42NW8xRm5aRWpoQnNKNnNlSGFad1MzOHNkdWtER0xQTFN5Z3lmRERsZnZWWE5CZEJneVRlMDd2VmNPMjloK0g5eCswZUVJTS9CRkFweHc5RUh6K1JocGN6clc1JmZtL3JhRE1sc0NMTFlpMVErRGtPcllvTGdldz0=; _ir=0"
+		}
+			}).then(({ data }) => {
+		const $ = cheerio.load(data)
+		const result = [];
+		const hasil = [];
+   		 $('div > a').get().map(b => {
+        const link = $(b).find('img').attr('src')
+            result.push(link)
+		});
+   		result.forEach(v => {
+		 if(v == undefined) return
+		 hasil.push(v.replace(/236/g,'736'))
+			})
+			hasil.shift();
+		resolve(hasil)
+		})
+	})
+                        }
+function searchsticker(queryy) {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://getstickerpack.com/stickers?query=${queryy}`)
+    .then(({data}) => {
+      const $ = cheerio.load(data)
+      const source = []
+      const linknya = []
+      $('#stickerPacks > div > div:nth-child(3) > div > a').each((a, b) => {
+        source.push($(b).attr('href'))
+      })
+      axios.get(source[Math.floor(Math.random() * source.length)])
+      .then(({data}) => {
+        const $2 = cheerio.load(data)
+        $2('#stickerPack > div > div.row > div > img').each((c, d) => {
+          linknya.push($2(d).attr('src').replace(/&d=200x200/g, ''))
+        })
+        result = {
+            title: $2('#intro > div > div > h1').text(),
+            stickerUrl: linknya
+        }
+        resolve(result)
+      })
+    }).catch(reject)
+  })
+}        
 async function capcut(url) {
   const response = await fetch(url);
   const data = await response.text();
@@ -75,4 +121,4 @@ async function chatgptss(message) {
     }
 }
 module.exports = { quotesAnime, 
-chatgptss, capcut}
+chatgptss, capcut, searchsticker, pinterest}
