6785a8ada0590b442e855d6f53ad78d436554d36
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-05 11:34:49 +0800

    index.js

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ app.get('/stats', (req, res) => {
 app.get('/loadihng', (req, res) => {
   res.sendFile(path.join(__dirname,  'index.html'));
 });
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname,  'profile.html'));
+});
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,  'home.html'));
 });
