85561d92c05342a0a75732ffc6fb68e7ddf5029a
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-05 10:52:43 +0800

    buffer.js

---
+++ function/scraper/buffer.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+exports.getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (err) {
+		return err
+	}
+}
+exports.fetchJson = fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
