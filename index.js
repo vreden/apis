662d60fc670c7727584bf2633fcf0ad3ef7c06f7
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-06 14:19:06 +0800

    index.js

--- index.js
+++ index.js
@@ -505,7 +505,7 @@ var requestSettings = {
         encoding: null
     };
     request(requestSettings, function (error, response, body) {
-        res.set('Content-Type', 'image/jpeg');
+        res.set('Content-Type', 'image/png');
         res.send(body);
     });    
 });
