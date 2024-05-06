580ff2fb6a20cbac043a962f6b29ac7ac902c89c
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-06 13:09:44 +0800

    exonityscraper.js

--- function/scraper/exonityscraper.js
+++ function/scraper/exonityscraper.js
@@ -120,5 +120,86 @@ async function chatgptss(message) {
         throw error;
     }
 }
+function PlayStore(search) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data, status } = await axios.get(`https://play.google.com/store/search?q=${search}&c=apps`)
+      const hasil = []
+      const $ = cheerio.load(data)
+      $('.ULeU3b > .VfPpkd-WsjYwc.VfPpkd-WsjYwc-OWXEXe-INsAgc.KC1dQ.Usd1Ac.AaN0Dd.Y8RQXd > .VfPpkd-aGsRMb > .VfPpkd-EScbFb-JIbuQc.TAQqTe > a').each((i, u) => {
+        const linkk = $(u).attr('href')
+        const nama = $(u).find('.j2FCNc > .cXFu1 > .ubGTjb > .DdYX5').text()
+        const developer = $(u).find('.j2FCNc > .cXFu1 > .ubGTjb > .wMUdtb').text()
+        const img = $(u).find('.j2FCNc > img').attr('src')
+        const rate = $(u).find('.j2FCNc > .cXFu1 > .ubGTjb > div').attr('aria-label')
+        const rate2 = $(u).find('.j2FCNc > .cXFu1 > .ubGTjb > div > span.w2kbF').text()
+        const link = `https://play.google.com${linkk}`
+
+        hasil.push({
+          link: link,
+          nama: nama ? nama : 'No name',
+          developer: developer ? developer : 'No Developer',
+          img: img ? img : 'https://i.ibb.co/G7CrCwN/404.png',
+          rate: rate ? rate : 'No Rate',
+          rate2: rate2 ? rate2 : 'No Rate',
+          link_dev: `https://play.google.com/store/apps/developer?id=${developer.split(" ").join('+')}`
+        })
+      })
+      if (hasil.every(x => x === undefined)) return resolve({ message: 'Tidak ada result!' })
+      resolve(hasil)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+}
+function BukaLapak(search) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await axios.get(`https://www.bukalapak.com/products?from=omnisearch&from_keyword_history=false&search[keywords]=${search}&search_source=omnisearch_keyword&source=navbar`, {
+        headers: {
+          "user-agent": 'Mozilla/ 5.0(Windows NT 10.0; Win64; x64; rv: 108.0) Gecko / 20100101 Firefox / 108.0'
+        }
+      })
+      const $ = cheerio.load(data);
+      const dat = [];
+      const b = $('a.slide > img').attr('src');
+      $('div.bl-flex-item.mb-8').each((i, u) => {
+        const a = $(u).find('observer-tracker > div > div');
+        const img = $(a).find('div > a > img').attr('src');
+        if (typeof img === 'undefined') return
+
+        const link = $(a).find('.bl-thumbnail--slider > div > a').attr('href');
+        const title = $(a).find('.bl-product-card__description-name > p > a').text().trim();
+        const harga = $(a).find('div.bl-product-card__description-price > p').text().trim();
+        const rating = $(a).find('div.bl-product-card__description-rating > p').text().trim();
+        const terjual = $(a).find('div.bl-product-card__description-rating-and-sold > p').text().trim();
+
+        const dari = $(a).find('div.bl-product-card__description-store > span:nth-child(1)').text().trim();
+        const seller = $(a).find('div.bl-product-card__description-store > span > a').text().trim();
+        const link_sel = $(a).find('div.bl-product-card__description-store > span > a').attr('href');
+
+        const res_ = {
+          title: title,
+          rating: rating ? rating : 'No rating yet',
+          terjual: terjual ? terjual : 'Not yet bought',
+          harga: harga,
+          image: img,
+          link: link,
+          store: {
+            lokasi: dari,
+            nama: seller,
+            link: link_sel
+          }
+        };
+
+        dat.push(res_);
+      })
+      if (dat.every(x => x === undefined)) return resolve({ message: 'Tidak ada result!' })
+      resolve(dat)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+		}
 module.exports = { quotesAnime, 
-chatgptss, capcut, searchsticker, pinterest}
+chatgptss, capcut, searchsticker, pinterest, PlayStore}
