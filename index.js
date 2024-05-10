c3a44daed0b7de945d03efb98ae5de0a423d4fb3
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-10 23:49:34 +0800

    index.js

--- index.js
+++ index.js
@@ -116,6 +116,40 @@ async function kobo(input) {
     throw error;
   }
 }
+// ml stalk
+async function mlstalk(id, zoneId) {
+    return new Promise(async (resolve, reject) => {
+      axios
+        .post(
+          'https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store',
+          new URLSearchParams(
+            Object.entries({
+              productId: '1',
+              itemId: '2',
+              catalogId: '57',
+              paymentId: '352',
+              gameId: id,
+              zoneId: zoneId,
+              product_ref: 'REG',
+              product_ref_denom: 'AE',
+            })
+          ),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Referer: 'https://www.duniagames.co.id/',
+              Accept: 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          resolve(response.data.data.gameDetail)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+}
 // ai kobo
 async function alicia(input) {
   const messages = [
@@ -438,6 +472,28 @@ app.get('/api/ttsearch', async (req, res) => {
       creator: "RIAN X EXONITY",
       result: { response }
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/mlstalk', async (req, res) => {
+  try {
+    const id = req.query.query;
+    if (!id) {
+      return res.status(400).json({ error: 'id nya mana?' });
+    }
+	  const zona = req.query.query;
+    if (!zona) {
+      return res.status(400).json({ error: 'Parameter "zonaid" tidak ditemukan' });
+    }
+    mlstalk(id, zona) 
+    .then((gameDetail) => {
+    res.status(200).json({
+      status: 200,
+      creator: "RIAN X EXONITY",
+      gameDetail
+    });
+    }) 
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
