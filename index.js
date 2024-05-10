f3158ea9822cabcdeaf0bd4a977467ec37571bcb
DANZ store <142219160+Putumc@users.noreply.github.com>
2024-05-10 21:41:30 +0800

    index.js

--- index.js
+++ index.js
@@ -667,7 +667,7 @@ var requestSettings = {
         encoding: null
     };
     request(requestSettings, function (error, response, body) {
-        res.set('Content-Type', 'image/png');
+        res.set('Content-Type', 'image/jpeg');
         res.send(body);
     });    
 });
