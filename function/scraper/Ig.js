dbd784bf899071276d6c609c05a70151364e0c86
Rian <142219160+Rianofc@users.noreply.github.com>
2024-05-12 15:06:17 +0800

    Ig.js

--- function/scraper/Ig.js
+++ function/scraper/Ig.js
@@ -1,21 +1,46 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function ig(match) {
-  const result = [];
-  const form = {
-    url: match,
-    submit: '',
-  };
-  const { data } = await axios.post('https://downloadgram.org/', form);
-  const $ = cheerio.load(data);
-  $('#downloadhere > a').each(function (a, b) {
-    const url = $(b).attr('href');
-    if (url) result.push(url);
+async function ig(url) {
+  let res = await axios("https://indown.io/");
+  let _$ = cheerio.load(res.data);
+  let referer = _$("input[name=referer]").val();
+  let locale = _$("input[name=locale]").val();
+  let _token = _$("input[name=_token]").val();
+  let { data } = await axios.post(
+    "https://indown.io/download",
+    new URLSearchParams({
+      link: url,
+      referer,
+      locale,
+      _token,
+    }),
+    {
+      headers: {
+        cookie: res.headers["set-cookie"].join("; "),
+      },
+    },
+  );
+  let $ = cheerio.load(data);
+  let result = [];
+  let __$ = cheerio.load($("#result").html());
+  __$("video").each(function () {
+    let $$ = $(this);
+    result.push({
+      type: "video",
+      thumbnail: $$.attr("poster"),
+      url: $$.find("source").attr("src"),
+    });
+  });
+  __$("img").each(function () {
+    let $$ = $(this);
+    result.push({
+      type: "image",
+      url: $$.attr("src"),
+    });
   });
 
   return result;
-
 }
 
 module.exports = {
