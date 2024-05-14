d1f2eacc5a035110cbbed53b16f8f2ff0df61b11
Rian <142219160+Rianofc@users.noreply.github.com>
2024-05-14 23:06:14 +0800

    index.js

--- index.js
+++ index.js
@@ -347,6 +347,38 @@ async function terabox(url) {
 
   return info;
 }
+function Gemini(key, apikey) {
+  this.conversation_id = '';
+  this.response_id = '';
+  this.choice_id = '';
+  this.image_url = null;
+  this.image_name = null;
+  this.tools = [];
+  this.params = { bl: '', _reqid: '', rt: 'c' };
+  this.data = { 'f.req': '', at: '' };
+  this.post_url = 'https://gemini.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate';
+  this.headers = this.setupHeaders(key, apikey);
+}
+
+Gemini.prototype.setupHeaders = function(key, apikey) {
+  return {
+    "Host": "gemini.google.com", "X-Same-Domain": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Origin": "https://gemini.google.com", "Referer": "https://gemini.google.com/",
+    "Cookie": `${key || '__Secure-1PSID'}=${apikey || 'g.a000gQhbTE4WvC7mwVL4CcWSxbt1Bde7Ady6qpt6951pafinWART4EEKmcskZMFX08uuSKwbvAACgYKAVYSAQASFQHGX2Mi1KAIQT0oz9dXZXKy0ioMBBoVAUF8yKpem3c3iJtHRDMQF3nSHOxU0076'}`
+  };
+};
+
+Gemini.prototype.question = function(query) {
+  const response = fetch('https://gemini.google.com/', { method: 'GET', headers: this.headers })
+    .then(response => response.text())
+    .then(geminiText => {
+      // Lakukan proses selanjutnya sesuai kebutuhan
+    })
+    .catch(error => {
+      console.error(error);
+      return { content: `Error: ${error.message}` };
+    });
+};
   var {
   ytDonlodMp3,
   ytDonlodMp4,
@@ -746,9 +778,8 @@ app.get('/api/gemini', async (req, res) => {
     if (!message) {
       return res.status(400).json({ error: 'Parameter "query" tidak ditemukan' });
     }
-   var response = await fetch(`https://apiruulzz.my.id/api/gemini?query=${message}`);
-    var data = await response.json();
-    var { result: result } = data;
+   const results = await Gemini(message) 
+	const result = results.geminiText
     res.status(200).json({
       status: 200,
       creator: "RIAN X EXONITY",
