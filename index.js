9e5f1087781f84ce3dd22ad5d0b179bb948b8e41
Rian <142219160+Rianofc@users.noreply.github.com>
2024-05-26 19:54:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2060,6 +2060,7 @@ app.get('/api/soundcloud', async (req, res) => {
     }
    var response = await fetch(`https://api.xyro.fund/api/soundcloud?url=${message}`);
     var result = await response.json();
+	  var { response: result } = data.data;
     res.status(200).json({
       status: 200,
       creator: "RIAN X EXONITY",
