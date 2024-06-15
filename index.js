6d2029ef29dde07def1c9239dc11c07658bd59e7
Rian <142219160+Rianofc@users.noreply.github.com>
2024-06-15 17:15:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1178,7 +1178,7 @@ async function morav2(prompt, username) {
       }),
     );
     const hours = date.getHours();
-    const name = await conn.getName(m.sender);
+    const name = ${username}
     const minutes = date.getMinutes();
     const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
 
