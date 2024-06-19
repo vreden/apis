69dd9344bb01126361b6a79b1cc1289bb9eb3142
Rian <142219160+Rianofc@users.noreply.github.com>
2024-06-19 19:14:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1378,7 +1378,7 @@ const response = await axios({
     url: "https://chateverywhere.app/api/chat",
     headers: {
       "Content-Type": "application/json",
-      "Cookie": "_ga=GA1.1.34196701.1707462626; _ga_ZYMW9SZKVK=GS1.1.1707462625.1.0.1707462625.60.0.0; ph_phc_9n85Ky3ZOEwVZlg68f8bI3jnOJkaV8oVGGJcoKfXyn1_posthog=%7B%22distinct_id%22%3A%225aa4878d-a9b6-40fb-8345-3d686d655483%22%2C%22%24sesid%22%3A%5B1707462733662%2C%22018d8cb4-0217-79f9-99ac-b77f18f82ac8%22%2C1707462623766%5D%7D",
+      "Cookie": "_ga=GA1.1.1784785033.1718795068; _ga_ZYMW9SZKVK=GS1.1.1718795067.1.1.1718795240.52.0.0; ph_phc_9n85Ky3ZOEwVZlg68f8bI3jnOJkaV8oVGGJcoKfXyn1_posthog=%7B%22distinct_id%22%3A%31d7fafe-1b13-4bc4-9e30-19ec363f7913%22%2C%22%24sesid%22%3A%5B1718795264950%2C%22018d8cb4-0217-79f9-99ac-b77f18f82ac8%22%2C1718795065801%5D%7D",
       Origin: "https://chateverywhere.app",
       Referer: "https://chateverywhere.app/id",
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
