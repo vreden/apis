f78bfe04db43e5f09f797f9e3a56f6f6ea6fc2a8
Syarif Muhammad Yahya Ali Almuthahar <157842317+vreden@users.noreply.github.com>
2024-07-10 00:55:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -469,7 +469,7 @@ async function youtubedlv2(url) {
       quality: k,
       fileSizeH: size,
       fileSize: parseFloat(size) * (/MB$/.test(size) ? 1000 : 1),
-      download: convertv2.bind(null, urlConvert, json.vid, 'mp4', k, json.token, parseInt(json.timeExpires), json.fn)
+      download: await convertv2(null, urlConvert, json.vid, 'mp4', k, json.token, parseInt(json.timeExpires), json.fn)
     };
   });
   const audio = {};
@@ -478,7 +478,7 @@ async function youtubedlv2(url) {
       quality: key,
       fileSizeH: size,
       fileSize: parseFloat(size) * (/MB$/.test(size) ? 1000 : 1),
-      download: convertv2.bind(null, urlConvert, json.vid, 'mp3', key.replace(/kbps/i, ''), json.token, parseInt(json.timeExpires), json.fn)
+      download: await convertv2(null, urlConvert, json.vid, 'mp3', key.replace(/kbps/i, ''), json.token, parseInt(json.timeExpires), json.fn)
     };
   });
   const res = {
