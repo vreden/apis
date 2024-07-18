88ac0cc796a03f841e98eefd29363e05371b36c0
Syarif Muhammad Yahya Ali Almuthahar <157842317+vreden@users.noreply.github.com>
2024-07-18 19:21:12 +0700

    Add files via upload

--- function/scraper/yt.js
+++ function/scraper/yt.js
@@ -1,22 +1,109 @@
 const yt = require("ytdl-core")
 const yts = require("yt-search")
+const axios = require('axios')
+
+class Ytdl {
+    constructor() {
+        this.baseUrl = 'https://id-y2mate.com';
+    }
+
+    async search(url) {
+        const requestData = new URLSearchParams({
+            k_query: url,
+            k_page: 'home',
+            hl: '',
+            q_auto: '0'
+        });
+
+        const requestHeaders = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept': '*/*',
+            'X-Requested-With': 'XMLHttpRequest'
+        };
+
+        try {
+            const response = await axios.post(`${this.baseUrl}/mates/analyzeV2/ajax`, requestData, {
+                headers: requestHeaders
+            });
+
+            const responseData = response.data;
+            console.log(responseData);
+            return responseData;
+        } catch (error) {
+            if (error.response) {
+                console.error(`HTTP error! status: ${error.response.status}`);
+            } else {
+                console.error('Axios error: ', error.message);
+            }
+        }
+    }
+
+    async convert(videoId, key) {
+        const requestData = new URLSearchParams({
+            vid: videoId,
+            k: key
+        });
+
+        const requestHeaders = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept': '*/*',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36',
+            'Referer': `${this.baseUrl}/youtube/${videoId}`
+        };
+
+        try {
+            const response = await axios.post(`${this.baseUrl}/mates/convertV2/index`, requestData, {
+                headers: requestHeaders
+            });
+
+            const responseData = response.data;
+            console.log(responseData);
+            return responseData;
+        } catch (error) {
+            if (error.response) {
+                console.error(`HTTP error! status: ${error.response.status}`);
+            } else {
+                console.error('Axios error: ', error.message);
+            }
+        }
+    }
+
+    async play(url) {
+        let { links, vid, title } = await this.search(url);
+        let video = {}, audio = {};
+
+        for (let i in links.mp4) {
+            let input = links.mp4[i];
+            let { fquality, dlink } = await this.convert(vid, input.k);
+            video[fquality] = {
+                size: input.q,
+                url: dlink
+            };
+        }
+
+        for (let i in links.mp3) {
+            let input = links.mp3[i];
+            let { fquality, dlink } = await this.convert(vid, input.k);
+            audio[fquality] = {
+                size: input.q,
+                url: dlink
+            };
+        }
+
+        return { title, video, audio };
+    }
+}
 
 async function ytDonlodMp3(url) {
   return new Promise((resolve, reject) => {
     try {
       const id = yt.getVideoID(url)
+      
       const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
       .then((data) => {
-        let pormat = data.formats
-        let audio = []
-        let size = []
-        for (let i = 0; i < pormat.length; i++) {
-          if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
-            let aud = pormat[i]
-            audio.push(aud.url)
-            size.push(aud.contentLength)
-          }
-        }
+        const yt2mate = new Ytdl();
+        const y2mate = await yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
         const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
         const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
         const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
@@ -29,8 +116,7 @@ async function ytDonlodMp3(url) {
           channel: channel,
           published: published,
           views: views,
-          url: audio[1],
-          size: size[1]
+          url: y2mate.audio["128"].url
         }
         return(result)
       })
@@ -48,16 +134,8 @@ async function ytDonlodMp4(url) {
       const id = yt.getVideoID(url)
       const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
       .then((data) => {
-        let pormat = data.formats
-        let video = []
-        let size = []
-        for (let i = 0; i < pormat.length; i++) {
-          if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
-            let vid = pormat[i]
-            video.push(vid.url)
-            size.push(vid.contentLength)
-          }
-        }
+        const yt2mate = new Ytdl();
+        const y2mate = await yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
         const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
         const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
         const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
@@ -70,8 +148,7 @@ async function ytDonlodMp4(url) {
           channel: channel,
           published: published,
           views: views,
-          url: video[0],
-          size: size[0]
+          url: y2mate.video["360"].url
         }
         return(result)
       })
@@ -99,16 +176,8 @@ async function ytPlayMp3(query) {
                 const id = yt.getVideoID(url[0])
                 const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
                 .then((data) => {
-                    let pormat = data.formats
-                    let audio = []
-                    let size = []
-                    for (let i = 0; i < pormat.length; i++) {
-                    if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
-                        let aud = pormat[i]
-                        audio.push(aud.url)
-            size.push(aud.contentLength)
-                    }
-                    }
+                    const yt2mate = new Ytdl();
+        const y2mate = await yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
                     const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
                     const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
                     const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
@@ -123,8 +192,7 @@ async function ytPlayMp3(query) {
                     channel: channel,
                     published: published,
                     views: views,
-                    url: audio[0],
-                    size: size[0]
+                    url: y2mate.audio["128"].url
                     }
                     return(result)
                 })
@@ -154,16 +222,8 @@ async function ytPlayMp4(query) {
                 const id = yt.getVideoID(url[0])
                 const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
                 .then((data) => {
-                    let pormat = data.formats
-                    let video = []
-                    let size = []
-                    for (let i = 0; i < pormat.length; i++) {
-                    if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
-                        let vid = pormat[i]
-                        video.push(vid.url)
-                        size.push(vid.contentLength)
-                    }
-                   }
+                    const yt2mate = new Ytdl();
+        const y2mate = await yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
                     const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
                     const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
                     const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
@@ -175,8 +235,7 @@ async function ytPlayMp4(query) {
                     channel: channel,
                     published: published,
                     views: views,
-                    url: video[0],
-                    size: size[0]
+                    url: y2mate.video["360"].url
                     }
                     return(result)
                 })
