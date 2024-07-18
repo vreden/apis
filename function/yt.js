7d01cc1d94f85ec4205b539cc31ef5d7621493b6
Syarif Muhammad Yahya Ali Almuthahar <157842317+vreden@users.noreply.github.com>
2024-07-18 20:03:52 +0700

    Add files via upload

---
+++ function/yt.js
@@ -0,0 +1,273 @@
+const yt = require("ytdl-core")
+const yts = require("yt-search")
+const axios = require('axios')
+
+class Ytdl {
+    constructor() {
+        this.baseUrl = 'https://id-y2mate.com';
+    }
+
+    async search(url) {
+        const requestData = new URLSearchParams({
+            k_query: url,
+            k_page: 'home',
+            hl: '',
+            q_auto: '0'
+        });
+
+        const requestHeaders = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept': '*/*',
+            'X-Requested-With': 'XMLHttpRequest'
+        };
+
+        try {
+            const response = await axios.post(`${this.baseUrl}/mates/analyzeV2/ajax`, requestData, {
+                headers: requestHeaders
+            });
+
+            const responseData = response.data;
+            console.log(responseData);
+            return responseData;
+        } catch (error) {
+            if (error.response) {
+                console.error(`HTTP error! status: ${error.response.status}`);
+            } else {
+                console.error('Axios error: ', error.message);
+            }
+        }
+    }
+
+    async convert(videoId, key) {
+        const requestData = new URLSearchParams({
+            vid: videoId,
+            k: key
+        });
+
+        const requestHeaders = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept': '*/*',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36',
+            'Referer': `${this.baseUrl}/youtube/${videoId}`
+        };
+
+        try {
+            const response = await axios.post(`${this.baseUrl}/mates/convertV2/index`, requestData, {
+                headers: requestHeaders
+            });
+
+            const responseData = response.data;
+            console.log(responseData);
+            return responseData;
+        } catch (error) {
+            if (error.response) {
+                console.error(`HTTP error! status: ${error.response.status}`);
+            } else {
+                console.error('Axios error: ', error.message);
+            }
+        }
+    }
+
+    async play(url) {
+        let { links, vid, title } = await this.search(url);
+        let video = {}, audio = {};
+
+        for (let i in links.mp4) {
+            let input = links.mp4[i];
+            let { fquality, dlink } = await this.convert(vid, input.k);
+            video[fquality] = {
+                size: input.q,
+                url: dlink
+            };
+        }
+
+        for (let i in links.mp3) {
+            let input = links.mp3[i];
+            let { fquality, dlink } = await this.convert(vid, input.k);
+            audio[fquality] = {
+                size: input.q,
+                url: dlink
+            };
+        }
+
+        return { title, video, audio };
+    }
+}
+
+async function ytDonlodMp3(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = yt.getVideoID(url)
+      const yt2mate = new Ytdl();
+        const y2mate = yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
+      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          title: title,
+          thumb: thumb,
+          channel: channel,
+          published: published,
+          views: views,
+          url: y2mate.audio.128.url
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
+
+async function ytDonlodMp4(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = yt.getVideoID(url)
+      const yt2mate = new Ytdl();
+        const y2mate = yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
+      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          title: title,
+          thumb: thumb,
+          channel: channel,
+          published: published,
+          views: views,
+          url: y2mate.video.360.url
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
+
+async function ytPlayMp3(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'video') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yt2mate = new Ytdl();
+        const y2mate = yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    status: true,
+                    code: 200,
+                    creator: 'Reyganz',
+                    title: title,
+                    thumb: thumb,
+                    channel: channel,
+                    published: published,
+                    views: views,
+                    url: y2mate.audio.128.url
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytPlayMp4(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'video') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yt2mate = new Ytdl();
+        const y2mate = yt2mate.play(`https://www.youtube.com/watch?v=${id}`)
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    title: title,
+                    thumb: thumb,
+                    channel: channel,
+                    published: published,
+                    views: views,
+                    url: y2mate.video.360.url
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+async function ytSearch(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const cari = yts(query)
+            .then((data) => {
+                res = data.all
+                return res
+            })
+            resolve(cari)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
+    })
+}
+
+module.exports = {
+  ytDonlodMp3,
+  ytDonlodMp4,
+  ytPlayMp3,
+  ytPlayMp4,
+  ytSearch
+};
